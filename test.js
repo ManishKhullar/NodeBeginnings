f19d1a1243404077f50cae89a8bf7684672c295e
Manish K <makchamp1@gmail.com>
2023-05-13 10:04:26 +0530

    2023 Q4 tasks

---
+++ test.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const server = http.createServer(
+    (request, response) => {
+       response.writeHead(200, {'Content-Type': 'text/text'})
+       response.end(straitLine)
+      })
+  server.listen(8000)
+
+  var straitLine = function sysTest()=>{
+    console.log(alert)
+    const alert = "SOS";
+    var sos = "LOL";
+  }
+
