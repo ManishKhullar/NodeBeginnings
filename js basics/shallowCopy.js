42706242acf97b3f137d5952029f3dd7e866100a
kromation <kromation@localhost.localdomain>
2025-04-22 23:00:04 +0530

    Deep Copy Shallow Copy

---
+++ js basics/shallowCopy.js
@@ -0,0 +1,7 @@
+const ingredientsList = ["noodles", {list:["eggs", "flour", "water"]}];
+const ingredientsListCopy = ingredientsList;
+
+
+// ????? DIfference between copying directly vs Copying using Array.from()
+console.log(ingredientsListCopy);
+console.log(ingredientsListCopy);
